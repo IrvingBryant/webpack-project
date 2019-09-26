989ee040bf977cc5ab0e0cbd174555b992ad9a13
Irving Yang <740465931@qq.com>
2019-09-26 11:54:53 +0800

    fix:

--- src/search/index.js
+++ src/search/index.js
@@ -1,45 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {a} from './tree-shaking'
-import './search.css'
-import './searchLess.less'
-import logo from '../img/logo.jpg'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { a } from './tree-shaking';
+import './search.css';
+import './searchLess.less';
+import logo from '../img/logo.jpg';
 
 class Search extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      Text:null
-    }
+    this.state = {
+      Text: null,
+    };
   }
 
-  handleLoading(){
-    //动态加载语法
-    import('./Text.js').then((Text)=>{
-      console.log(Text.default())
+  handleLoading() {
+    // 动态加载语法
+    import('./Text.js').then((Text) => {
+      console.log(Text.default());
       this.setState({
-        Text:Text.default()
-      })
-    })
+        Text: Text.default(),
+      });
+    });
   }
 
   render() {
-    const {Text} = this.state
+    const { Text } = this.state;
     return (
-      <div className='a search-Text'>
+      <div className="a search-Text">
         {/* {Text ?  <Text/> : null} */}
-        <img  src={logo} style={{
-          width:100,
-          height : 100
-        }}
-        onClick={()=>{
-          this.handleLoading()
-        }}/>  
+        <img
+          src={logo}
+          style={{
+            width: 100,
+            height: 100,
+          }}
+          onClick={() => {
+            this.handleLoading();
+          }}
+        />
       </div>
-    )
+    );
   }
 }
 ReactDOM.render(
   <Search />,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
